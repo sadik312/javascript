8b73c11605e379744c1a477502500e83ae57b5c2
sadik312 <sadikislam46@gmail.com>
2023-08-26 00:33:03 +0100

    completed move method

--- main.js
+++ main.js
@@ -45,6 +45,14 @@ class VillageState {
     }
 
     move(destination) {
-        if (!roadGraph)
+        if (!roadGraph[this.place].includes(destination)) {
+            return this;
+        } else {
+            let parcels = this.parcels.map(p => {
+                if (p.place != this.place) return p;
+                return {place: destination, address: p.address};
+            }).filter(p => p.place != p.address);
+            return new VillageState(destination, parcels);
+        }
     }
 }
