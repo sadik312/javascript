33ee78247fc06f70298200f456234664bdc6e40c
sadik312 <sadikislam46@gmail.com>
2023-08-28 01:01:22 +0100

    added randomRobot function

--- main.js
+++ main.js
@@ -91,3 +91,7 @@ function randomPick(array) {
     let choice = Math.floor(Math.random() * array.length);
     return array[choice];
 }
+
+function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])};
+}
