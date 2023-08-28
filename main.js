9d1fd33f36b7432b4ecb2160600584d48182129b
sadik312 <sadikislam46@gmail.com>
2023-08-28 12:59:36 +0100

    created static method for new state with parcels

--- main.js
+++ main.js
@@ -95,3 +95,16 @@ function randomPick(array) {
 function randomRobot(state) {
     return {direction: randomPick(roadGraph[state.place])};
 }
+
+VillageState.random = function(parcelCount = 5) {
+    let parcels = [];
+    for (let i=0; i<parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = this.randomPick(Object.apply.keys(roadGraph));
+        } while (place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState("Post Office ", parcels);
+};
