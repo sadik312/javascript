abf623491a9049d37f0179543d3b84b00108cb54
sadik312 <sadikislam46@gmail.com>
2023-09-09 00:12:51 +0100

    recursive approach

--- binary_search.js
+++ binary_search.js
@@ -2,4 +2,14 @@
 /*
 Searching technique which works on Divide and Conquer approach
 It is used to search for elements in a sorted array
+2 types of approaches: Iterative and Recursive
+*/
+
+/*
+Recursive Approach:
+1. Base Condition: if starting index is greater than ending index, return false
+2. Compute the middle index
+3. Compare middle with number x, if equal, return true
+4. If greater, call the same function with ending index = middle-1 and repeat step 1
+5. If smaller, call the same function with starting index = middle+1 and repeat step 1
 */
