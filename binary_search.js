a229641feccd690fa0f81f5c1e63537d9131ca2a
sadik312 <sadikislam46@gmail.com>
2023-09-09 00:27:05 +0100

    complete cursive bin search

--- binary_search.js
+++ binary_search.js
@@ -15,30 +15,45 @@ Recursive Approach:
 */
 
 let recursiveFunction = function (arr, x, start, end) {
-   // Base Condition
-   if (start > end){
-    return false;
-   } 
+    // Base Condition
+    if (start > end) {
+        return false;
+    }
 
-   // Find middle index
-   let mid = Math.floor((start + end)/2);
+    // Find middle index
+    let mid = Math.floor((start + end) / 2);
 
-   // Compare mid with given key x
-   if (arr[mid]===x) {
-    return true;
-   }
+    // Compare mid with given key x
+    if (arr[mid] === x) {
+        return true;
+    }
 
-   // If element at mid is greater than x,
-   // search LEFT half of mid
-   if (arr[mid] > x) {
-    return recursiveFunction(arr, x, start, mid-1);
-   } else {
-    // If element at mid is smaller than x,
-    // search RIGHT half of mid
-    return recursiveFunction(arr, x, mid+1, end);
-   }
+    // If element at mid is greater than x,
+    // search LEFT half of mid
+    if (arr[mid] > x) {
+        return recursiveFunction(arr, x, start, mid - 1);
+    } else {
+        // If element at mid is smaller than x,
+        // search RIGHT half of mid
+        return recursiveFunction(arr, x, mid + 1, end);
+    }
 
-   // Driver code
-   let arr = [1, 3, 5, 7, 9];
-   let x = 5;
-}
+    // Driver code
+    let arr = [1, 3, 5, 7, 9];
+    let x = 5;
+
+    if (recursiveFunction(arr, x, 0, arr.length - 1)) {
+        document.write("Element found!<br>");
+    } else {
+        document.write("Element not found!<br>");
+    }
+
+    x = 6;
+
+    if (recursiveFunction(arr, x, 0, arr.length-1)) {
+        document.write("Element found!<br>");
+    } else {
+        document.write("Element not found!<br>");
+    }
+
+}								
