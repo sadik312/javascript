b0d87d870b790c0165a18341a9ac9383ee4d18d6
sadik312 <sadikislam46@gmail.com>
2023-09-10 23:15:23 +0100

    redone selection sort

--- algorithms/selection_sort/selection_sort.js
+++ algorithms/selection_sort/selection_sort.js
@@ -1,36 +1,21 @@
-// Selection Sort:
-function swap(arr, xp, yp) {
-    var temp = arr[xp];
-    arr[xp] = arr[yp];
-    arr[yp] = temp;
-}
+// Selection Sort
 
-function selectionSort(arr, n) {
-    var i, j, min_idx;
+function selctionSort(arr) {
+    let n = arr.length;
 
-    // One by one move boundary of unsorted array
-    for (i=0; i<n-1; i++)
-    {
-        // Find the minimum element in unsorted subarray
-        min_idx = i;
-        for (j=i+1; j<n; j++)
-        if (arr[j] < arr[min_idx]){
-            min_idx = j;
+    for (let i=0; i<n; i++) {
+        // Finding smallest element in subarray
+        let min = i;
+        for (let j=i+1; j<n; j++) {
+            if (arr[j] < arr[min]) {
+                min=j;
+            }
         }
-
-            // Swap found minimuj element with first element
-            swap(arr, min_idx, i);
+        if (min != i) {
+            // Swapping elments
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
     }
 }
-
-function printArray(arr, size)
-{
-    var i;
-    for (i=0; i<size; i++) {
-        console.log(arr[i] + " ");
-    }
-}
-
-// Driver code
-var arr = [64, 25, 12, 22, 11];
-
